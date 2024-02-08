3c883887707a8e8f74bae8c006de3b70b8df92b7
Amir Shkolnik <amirshkolnik1@gmail.com>
2024-02-08 14:51:11 +0000

    add new file categories.py - five groups animals, fruits, countries, flowers, languages

---
+++ categories.py
@@ -0,0 +1,60 @@
+# List with 100 animals
+
+animals = [
+    "dog", "cat", "elephant", "lion", "tiger", "zebra", "giraffe", "hippo", "rhino", "cheetah",
+    "monkey", "gorilla", "kangaroo", "koala", "panda", "penguin", "bear", "wolf", "fox", "deer",
+    "buffalo", "camel", "gazelle", "moose", "elk", "reindeer", "crocodile", "alligator", "snake", "python",
+    "cobra", "anaconda", "lizard", "gecko", "iguana", "turtle", "tortoise", "frog", "toad", "salamander",
+    "newt", "axolotl", "goldfish", "shark", "whale", "dolphin", "octopus", "squid", "jellyfish", "starfish",
+    "seahorse", "crab", "lobster", "shrimp", "oyster", "clam", "snail", "slug", "bee", "ant", "wasp",
+    "butterfly", "moth", "dragonfly", "grasshopper", "cricket", "beetle", "ladybug", "spider", "scorpion",
+    "centipede", "millipede", "fly", "mosquito", "caterpillar", "larva", "butterfly", "prayingmantis",
+    "armadillo", "anteater", "sloth", "hedgehog", "porcupine", "chipmunk", "squirrel", "rabbit", "mouse",
+    "rat", "hamster", "guineapig", "gerbil", "ferret", "mole", "weasel", "otter", "badger", "raccoon",
+    "skunk", "opossum", "platypus", "echidna"
+    
+]
+
+# List with 50 countries
+
+countries = [
+        "Brazil", "Japan", "France", "Italy", "Germany", "Canada", "Australia", "Mexico", "India", "China",
+        "Spain", "Russia", "Argentina", "SouthAfrica", "SouthKorea", "Netherlands", "Switzerland", "Sweden",
+        "Portugal", "Belgium", "Norway", "Denmark", "Finland", "Greece", "Egypt", "Turkey", "Thailand", "Iran",
+        "Vietnam", "Poland", "Ukraine", "Indonesia", "Philippines", "Malaysia", "Israel", "SaudiArabia",
+        "Pakistan", "Bangladesh", "Nigeria", "Kenya", "Morocco", "Peru", "Chile", "Colombia", "Venezuela",
+        "Cuba", "NewZealand", "Ireland", "Austria", "Hungary"
+
+]
+
+# List with 50 flowers
+
+flowers = [
+    "rose", "tulip", "daisy", "lily", "sunflower", "orchid", "daffodil", "peonies", "hydrangea", "carnation",
+    "poppy", "marigold", "lavender", "chrysanthemum", "hibiscus", "gerbera", "snapdragon", "freesia", "iris",
+    "aster", "zinnia", "dahlia", "crocus", "azalea", "anemone", "cosmos", "lilac", "hyacinth", "geranium",
+    "pansy", "forgetmenot", "bougainvillea", "camellia", "ranunculus", "gladiolus", "fuchsia", "birdofparadise",
+    "buttercup", "columbine", "delphinium", "foxglove", "gardenia", "larkspur", "narcissus", "peony",
+    "sweetpea", "tigerlily", "verbena", "wisteria"
+    
+]
+
+# List with 50 languages
+
+languages = [
+    "English", "Spanish", "French", "German", "Italian",
+    "Portuguese", "Russian", "Japanese", "Chinese", "Arabic",
+    "Hindi", "Bengali", "Urdu", "Indonesian", "Korean",
+    "Turkish", "Dutch", "Swedish", "Greek", "Polish",
+    "Vietnamese", "Thai", "Finnish", "Norwegian", "Danish",
+    "Romanian", "Czech", "Hungarian", "Slovak", "Croatian",
+    "Serbian", "Bulgarian", "Ukrainian", "Malay", "Tagalog",
+    "Lithuanian", "Latvian", "Estonian", "Albanian", "Slovenian",
+    "Macedonian", "Swahili", "Hausa", "Yoruba", "Zulu",
+    "Amharic", "Somali", "Gujarati", "Punjabi"
+]
+
+
+
+
+
