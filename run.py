9ed76b012e34ee342a2269a09e4e7e2d87e49293
Amir Shkolnik <amirshkolnik1@gmail.com>
2024-02-09 14:03:46 +0000

    run - fix error random word not recognized add chosen_category, chosen_list = choose_category() to def hangman

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
-# List with 100 animals
+import random
+import string
 
 animals = [
     "dog", "cat", "elephant", "lion", "tiger", "zebra", "giraffe", "hippo", "rhino", "cheetah",
@@ -12,10 +13,7 @@ animals = [
     "armadillo", "anteater", "sloth", "hedgehog", "porcupine", "chipmunk", "squirrel", "rabbit", "mouse",
     "rat", "hamster", "guineapig", "gerbil", "ferret", "mole", "weasel", "otter", "badger", "raccoon",
     "skunk", "opossum", "platypus", "echidna"
-    
 ]
-
-# List with 50 countries
 
 countries = [
         "Brazil", "Japan", "France", "Italy", "Germany", "Canada", "Australia", "Mexico", "India", "China",
@@ -24,10 +22,7 @@ countries = [
         "Vietnam", "Poland", "Ukraine", "Indonesia", "Philippines", "Malaysia", "Israel", "SaudiArabia",
         "Pakistan", "Bangladesh", "Nigeria", "Kenya", "Morocco", "Peru", "Chile", "Colombia", "Venezuela",
         "Cuba", "NewZealand", "Ireland", "Austria", "Hungary"
-
 ]
-
-# List with 50 flowers
 
 flowers = [
     "rose", "tulip", "daisy", "lily", "sunflower", "orchid", "daffodil", "peonies", "hydrangea", "carnation",
@@ -36,10 +31,7 @@ flowers = [
     "pansy", "forgetmenot", "bougainvillea", "camellia", "ranunculus", "gladiolus", "fuchsia", "birdofparadise",
     "buttercup", "columbine", "delphinium", "foxglove", "gardenia", "larkspur", "narcissus", "peony",
     "sweetpea", "tigerlily", "verbena", "wisteria"
-    
 ]
-
-# List with 50 languages
 
 languages = [
     "English", "Spanish", "French", "German", "Italian",
@@ -54,8 +46,6 @@ languages = [
     "Amharic", "Somali", "Gujarati", "Punjabi"
 ]
 
-# List with 50 fruits
-
 fruits = [
     "apple", "banana", "orange", "grape", "strawberry",
     "watermelon", "blueberry", "mango", "pineapple", "kiwi",
@@ -68,9 +58,6 @@ fruits = [
     "quince", "plantain", "starfruit", "honeydew", "rhubarb",
     "gooseberry", "soursop", "kumquat", "elderberry", "ackee"
 ]
-
-import random
-import string
 
 categories = {
     "Animals": animals,
@@ -91,28 +78,16 @@ def choose_category():
         print("Invalid choice. Please try again.")
         return choose_category()  # Recursive call to prompt the user again
     else:
-        print("You chose ")
         chosen_category = list(categories.keys())[choice]
-        chosen_list = categories[chosen_category]
+        print("You chose", chosen_category + ".", "Great choice!")  # Print the chosen category here
+        chosen_list = categories[chosen_category] #chosen_list refers to a list of words associated with the category that the user has chosen to play with.
         return chosen_category, chosen_list
-
-def get_valid_word(words_list):
-    word = random.choice(words_list)
-    while '-' in word or ' ' in word:
-        word = random.choice(words_list)
-    return word.upper()
-
-
-def get_valid_word(words_list):
-    word = random.choice(fruits)  # randomly choose a fruit from the fruits list
-    while '-' in word or ' ' in word:
-        word = random.choice(fruits)
-    return word.upper()
+       
 
 def display_hangman(mistakes):
     hangman_stages = [
         "",
-       "      _____ \n     |      \n     |      \n     |      \n     |      \n     |      \n     |      \n   __|_________      ",
+        "      _____ \n     |      \n     |      \n     |      \n     |      \n     |      \n     |      \n   __|_________      ",
         "      _____  \n     |     | \n     |       \n     |       \n     |       \n     |       \n     |       \n   __|_________ ",
         "      _____ \n     |     | \n     |     O \n     |       \n     |       \n     |       \n     |       \n   __|_________ ",
         "      _____   \n     |     |  \n     |     O  \n     |     |  \n     |        \n     |        \n     |        \n   __|_________ ",
@@ -121,45 +96,42 @@ def display_hangman(mistakes):
         "      _____    \n     |     |  \n     |     O  \n     |    /|\\ \n     |    /   \n     |        \n     |        \n   __|_________ ",
         "      _____    \n     |     |  \n     |     O  \n     |    /|\\ \n     |    / \\ \n     |        \n     |        \n   __|_________ "
     ]
-    
+
     print(hangman_stages[mistakes])
 
+def chosen_category_word(chosen_list):
+    word = random.choice(chosen_list)
+    while '-' in word or ' ' in word:
+        word = random.choice(chosen_list) # randomly choose a word fron the chosen category
+    return word.upper()
+
 def hangman():
-    word = get_valid_word(fruits)
-    word_letters = set(word)    # letters in the word
+    chosen_category, chosen_list = choose_category()
+    word = chosen_category_word(chosen_list)
+    word_letters = set(word)
     alphabet = set(string.ascii_uppercase)
-    used_letters = set()    # what the user has guessed
-
+    used_letters = set()
     mistakes = 0
-    # getting user input
+
     while len(word_letters) > 0 and mistakes < 7:
-        # display hangman
         display_hangman(mistakes)
-
-        # letters used
-        # ' '.join(['a', 'b',  'cd']) --> 'a b cd'
         print('You have', 7 - mistakes, 'lives left and you have used these letters:', ' '.join(used_letters))
-
-        # what current word is (ie W - R D)
         word_list = [letter if letter in used_letters else '-' for letter in word]
         print('Current word:', ' '.join(word_list))
 
-        user_letter = input('Guess a letter: ').upper()
+        user_letter = input('Guess a letter: \n').upper()
         if user_letter in alphabet - used_letters:
             used_letters.add(user_letter)
             if user_letter in word_letters:
                 word_letters.remove(user_letter)
             else:
-                mistakes += 1  # increment mistake count if wrong
+                mistakes += 1
                 print('Letter is not in word.')
-
         elif user_letter in used_letters:
             print('You have already used that character. Please try again.')
-
         else:
             print('Invalid character. Please try again.')
 
-    # gets here when len(word_letters) == 0 OR when mistakes == 7
     if mistakes == 7:
         display_hangman(mistakes)
         print('You died, sorry. The word was', word)
@@ -167,21 +139,19 @@ def hangman():
         print('You guessed the word', word, '!!')
 
 def continue_game():
-    play_game = True
-    while play_game:
-        continue_playing = input("Would you like to continue playing the game? y/n ")
-        
+    while True:
+        continue_playing = input("Would you like to continue playing the game? (y/n): \n")
         if continue_playing.lower() == "y":
             print("You have decided to continue playing the game.")
-            hangman() # restart the game if user chooses to continue
+            hangman()
         elif continue_playing.lower() == "n":
             print("Now closing the game...")
-            play_game = False
+            break
         else:
             print("That is not a valid option. Please try again.")
 
     print("Thanks for playing")
 
-choose_category()
+
 hangman()
 continue_game()
