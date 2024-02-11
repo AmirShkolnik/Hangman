21bf448da4833b90cf2f6af5f9365744cdc6080b
Amir Shkolnik <amirshkolnik1@gmail.com>
2024-02-11 09:58:33 +0000

    run - change text continue_game

--- categories.py
+++ categories.py
@@ -1,4 +1,10 @@
-# List with 100 animals
+categories = {
+    "Animals": animals,
+    "Countries": countries,
+    "Flowers": flowers,
+    "Languages": languages,
+    "Fruits": fruits
+}
 
 animals = [
     "dog", "cat", "elephant", "lion", "tiger", "zebra", "giraffe", "hippo", "rhino", "cheetah",
@@ -8,14 +14,11 @@ animals = [
     "newt", "axolotl", "goldfish", "shark", "whale", "dolphin", "octopus", "squid", "jellyfish", "starfish",
     "seahorse", "crab", "lobster", "shrimp", "oyster", "clam", "snail", "slug", "bee", "ant", "wasp",
     "butterfly", "moth", "dragonfly", "grasshopper", "cricket", "beetle", "ladybug", "spider", "scorpion",
-    "centipede", "millipede", "fly", "mosquito", "caterpillar", "larva", "butterfly", "prayingmantis",
+    "centipede", "millipede", "fly", "mosquito", "caterpillar", "larva", "butterfly", "mantis",
     "armadillo", "anteater", "sloth", "hedgehog", "porcupine", "chipmunk", "squirrel", "rabbit", "mouse",
     "rat", "hamster", "guineapig", "gerbil", "ferret", "mole", "weasel", "otter", "badger", "raccoon",
     "skunk", "opossum", "platypus", "echidna"
-    
 ]
-
-# List with 50 countries
 
 countries = [
         "Brazil", "Japan", "France", "Italy", "Germany", "Canada", "Australia", "Mexico", "India", "China",
@@ -24,10 +27,7 @@ countries = [
         "Vietnam", "Poland", "Ukraine", "Indonesia", "Philippines", "Malaysia", "Israel", "SaudiArabia",
         "Pakistan", "Bangladesh", "Nigeria", "Kenya", "Morocco", "Peru", "Chile", "Colombia", "Venezuela",
         "Cuba", "NewZealand", "Ireland", "Austria", "Hungary"
-
 ]
-
-# List with 50 flowers
 
 flowers = [
     "rose", "tulip", "daisy", "lily", "sunflower", "orchid", "daffodil", "peonies", "hydrangea", "carnation",
@@ -36,10 +36,7 @@ flowers = [
     "pansy", "forgetmenot", "bougainvillea", "camellia", "ranunculus", "gladiolus", "fuchsia", "birdofparadise",
     "buttercup", "columbine", "delphinium", "foxglove", "gardenia", "larkspur", "narcissus", "peony",
     "sweetpea", "tigerlily", "verbena", "wisteria"
-    
 ]
-
-# List with 50 languages
 
 languages = [
     "English", "Spanish", "French", "German", "Italian",
@@ -54,7 +51,15 @@ languages = [
     "Amharic", "Somali", "Gujarati", "Punjabi"
 ]
 
-
-
-
-
+fruits = [
+    "apple", "banana", "orange", "grape", "strawberry",
+    "watermelon", "blueberry", "mango", "pineapple", "kiwi",
+    "peach", "pear", "cherry", "plum", "raspberry",
+    "lemon", "lime", "apricot", "coconut", "fig",
+    "avocado", "pomegranate", "cantaloupe", "cranberry", "blackberry",
+    "guava", "papaya", "lychee", "passionfruit", "date",
+    "kiwifruit", "tangerine", "nectarine", "persimmon", "dragonfruit",
+    "grapefruit", "mandarin", "boysenberry", "mulberry", "clementine",
+    "quince", "plantain", "starfruit", "honeydew", "rhubarb",
+    "gooseberry", "soursop", "kumquat", "elderberry", "ackee"
+]
